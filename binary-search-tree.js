a02a85c470588618763e1adc658f7012b32f529d
castawaypirate <apollonas@csd.auth.gr>
2026-05-20 06:50:30 +0300

    Implement core BST operations, traversals, and balancing methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -4,17 +4,41 @@ class Node {
     this.left = left;
     this.right = right;
   }
+
+  hasChildren() {
+    return this.left || this.right;
+  }
+
+  hasBothChildren() {
+    return this.left && this.right;
+  }
+
+  theOnlyChild() {
+    if (this.hasBothChildren()) {
+      return false;
+    }
+    if (!this.left) {
+      return this.right;
+    }
+    if (!this.right) {
+      return this.left;
+    }
+    return false;
+  }
 }
 
 export default class Tree {
   constructor(arr) {
     this.root = this.#buildTree(this.#dedupeAndSort(arr));
+    this.size = 0;
   }
 
   #buildTree(arr) {
     if (arr.length === 0) {
       return null;
     }
+
+    this.size = arr.length;
 
     if (arr.length === 1) {
       return new Node(arr[0]);
@@ -32,6 +56,365 @@ export default class Tree {
   #dedupeAndSort(arr) {
     let mod = arr.filter((item, index) => arr.indexOf(item) === index);
     return mod.sort((a, b) => a - b);
+  }
+
+  includes(value) {
+    let current = this.root;
+    while (current) {
+      if (current.data === value) {
+        return true;
+      } else if (current.data > value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+
+  insert(value) {
+    let current = this.root;
+    if (!current) {
+      this.size++;
+      this.root = new Node(value);
+      return;
+    }
+    do {
+      if (current.data === value) {
+        return;
+      } else if (current.data > value) {
+        if (!current.left) {
+          this.size++;
+          current.left = new Node(value);
+          break;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          this.size++;
+          current.right = new Node(value);
+          break;
+        }
+        current = current.right;
+      }
+    } while (current);
+  }
+
+  deleteItem(value) {
+    if (!this.root) {
+      return;
+    }
+
+    if (this.root.data === value && this.size === 1) {
+      this.root = null;
+      return;
+    }
+
+    let parent = null;
+    let current = this.root;
+    while (current) {
+      if (current.data === value) {
+        this.size--;
+        break;
+      } else if (current.data > value) {
+        parent = current;
+        current = current.left;
+      } else {
+        parent = current;
+        current = current.right;
+      }
+    }
+
+    if (!current) {
+      return;
+    }
+
+    if (parent.left === current) {
+      if (current.hasChildren()) {
+        let onlyChild = current.theOnlyChild();
+        if (onlyChild) {
+          parent.left = onlyChild;
+        } else {
+          parent.left.data = this.#deleteSuccessor(current.right);
+        }
+      } else {
+        parent.left = null;
+      }
+    } else {
+      if (current.hasChildren()) {
+        let onlyChild = current.theOnlyChild();
+        if (onlyChild) {
+          parent.right = onlyChild;
+        } else {
+          parent.right.data = this.#deleteSuccessor(current.right);
+        }
+      } else {
+        parent.right = null;
+      }
+    }
+  }
+
+  #deleteSuccessor(node) {
+    let parent = null;
+    while (node.left) {
+      parent = node;
+      node = node.left;
+    }
+    let data = node.data;
+    parent.left = null;
+    return data;
+  }
+
+  leverOrderForEach(callback) {
+    if (typeof callback !== "function") {
+      throw new Error("Callback should be a function");
+    }
+    if (!this.root) {
+      return;
+    }
+    let que = [];
+    que.push(this.root);
+    while (que.length > 0) {
+      let first = que.shift();
+      callback(first.data);
+      if (first.left) {
+        que.push(first.left);
+      }
+      if (first.right) {
+        que.push(first.right);
+      }
+    }
+  }
+
+  preOrderForEach(callback) {
+    if (typeof callback !== "function") {
+      throw new Error("Callback should be a function");
+    }
+    if (!this.root) {
+      return;
+    }
+    let que = [];
+    que.unshift(this.root);
+    while (que.length > 0) {
+      let first = que.shift();
+      callback(first.data);
+      if (first.right) {
+        que.unshift(first.right);
+      }
+      if (first.left) {
+        que.unshift(first.left);
+      }
+    }
+  }
+
+  inOrderForEach(callback) {
+    if (typeof callback !== "function") {
+      throw new Error("Callback should be a function");
+    }
+
+    if (!this.root) {
+      return;
+    }
+
+    let que = [];
+    let euq = [];
+    que.unshift(this.root);
+
+    while (que.length > 0) {
+      let first = que.shift();
+      if (!first.left) {
+        euq.push(first);
+        callback(first.data);
+        if (first.right) {
+          que.unshift(first.right);
+        }
+      } else {
+        if (
+          first.right &&
+          !que.includes(first.right) &&
+          !euq.includes(first.right)
+        ) {
+          que.unshift(first.right);
+        }
+        if (!euq.includes(first.left)) {
+          que.unshift(first);
+        }
+        if (first.left) {
+          if (euq.includes(first.left)) {
+            euq.push(first);
+            callback(first.data);
+          } else {
+            que.unshift(first.left);
+          }
+        }
+      }
+    }
+  }
+
+  postOrderForEach(callback) {
+    if (typeof callback !== "function") {
+      throw new Error("Callback should be a function");
+    }
+
+    if (!this.root) {
+      return;
+    }
+
+    let que = [];
+    let euq = [];
+    que.unshift(this.root);
+
+    while (que.length > 0) {
+      let first = que.shift();
+      if (!first.hasChildren()) {
+        callback(first.data);
+        euq.push(first);
+      } else {
+        if (!euq.includes(first.left) && !euq.includes(first.right)) {
+          que.unshift(first);
+        }
+
+        let onlyChild = first.theOnlyChild();
+        if (onlyChild) {
+          if (euq.includes(onlyChild)) {
+            callback(first.data);
+            euq.push(first);
+          }
+        } else {
+          if (
+            euq.includes(first.left) &&
+            euq.includes(first.right) &&
+            !euq.includes(first)
+          ) {
+            callback(first.data);
+            euq.push(first);
+          }
+        }
+
+        if (
+          first.right &&
+          !que.includes(first.right) &&
+          !euq.includes(first.right)
+        ) {
+          que.unshift(first.right);
+        }
+
+        if (
+          first.left &&
+          !que.includes(first.left) &&
+          !euq.includes(first.left)
+        ) {
+          que.unshift(first.left);
+        }
+      }
+    }
+  }
+
+  height(value) {
+    let current = this.root;
+    while (current) {
+      if (current.data === value) {
+        break;
+      } else if (current.data > value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    if (!current) {
+      return;
+    }
+    let heights = {};
+    let que = [];
+    que.unshift(current);
+    heights[current.data] = 0;
+    while (que.length > 0) {
+      let first = que.shift();
+      if (first.left) {
+        que.unshift(first.left);
+        heights[first.left.data] = heights[first.data] + 1;
+      }
+      if (first.right) {
+        que.unshift(first.right);
+        heights[first.right.data] = heights[first.data] + 1;
+      }
+    }
+    return Math.max(...Object.values(heights));
+  }
+
+  depth(value) {
+    let current = this.root;
+    let d = 0;
+    while (current) {
+      if (current.data === value) {
+        break;
+      } else if (current.data > value) {
+        d++;
+        current = current.left;
+      } else {
+        d++;
+        current = current.right;
+      }
+    }
+    if (current) {
+      return d;
+    }
+    return;
+  }
+
+  isBalanced() {
+    let current = this.root;
+    if (!current) {
+      return true;
+    }
+    let children = {};
+    let que = [];
+    que.unshift(current);
+    while (que.length > 0) {
+      let first = que.shift();
+      children[first.data] = [];
+      if (first.left) {
+        que.unshift(first.left);
+        children[first.data].push(first.left.data);
+      }
+      if (first.right) {
+        que.unshift(first.right);
+        children[first.data].push(first.right.data);
+      }
+    }
+
+    for (let v of Object.values(children)) {
+      let height0 = this.height(v[0]);
+      let height1 = 0;
+
+      let parentHeight0 = height0 + 1;
+      let parentHeight1 = 0;
+
+      if (v.length === 2) {
+        height1 = this.height(v[1]);
+        parentHeight1 = height1 + 1;
+      }
+      if (
+        Math.abs(height1 - height0) > 1 ||
+        Math.abs(parentHeight0 - parentHeight1) > 1
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  rebalance() {
+    let temp = [];
+    if (!this.root) {
+      return;
+    }
+
+    this.inOrderForEach((x) => {
+      temp.push(x);
+    });
+
+    this.root = this.#buildTree(temp);
   }
 
   prettyPrint(node = this.root, prefix = "", isLeft = true) {
--- main.js
+++ main.js
@@ -1,6 +1,89 @@
 import Tree from "./binary-search-tree.js";
 
-const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-// const arr = [1, 2, 3];
+const generateRandomArray = (length, min, max) => {
+  let temp = [];
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  for (let i = 0; i < length; i++) {
+    temp.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+  return temp;
+};
+
+let arr = generateRandomArray(20, 0, 100);
 let bst = new Tree(arr);
+
+console.log("Initial Tree:");
+bst.prettyPrint();
+console.log("Is balanced: " + bst.isBalanced());
+
+console.log("\n--- Level Order ---");
+bst.leverOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\n--- Pre Order ---");
+bst.preOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\n--- In Order ---");
+bst.inOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\n--- Post Order ---");
+bst.postOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\nUnbalancing tree...");
+bst.insert(230);
+bst.insert(690);
+bst.insert(620);
+bst.insert(1020);
+
+console.log("Is balanced: " + bst.isBalanced());
+
+console.log("\nRebalancing tree...");
+bst.rebalance();
+
+console.log("Is balanced: " + bst.isBalanced());
+
+console.log("\n--- Level Order (After Rebalance) ---");
+bst.leverOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\n--- Pre Order (After Rebalance) ---");
+bst.preOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\n--- In Order (After Rebalance) ---");
+bst.inOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\n--- Post Order (After Rebalance) ---");
+bst.postOrderForEach((x) => {
+  console.log(x);
+});
+
+console.log("\n--- Method Checks ---");
+console.log("Includes 690: " + bst.includes(690));
+console.log("Includes 9999: " + bst.includes(9999));
+
+console.log("Height of root: " + bst.height(bst.root.data));
+console.log("Depth of root: " + bst.depth(bst.root.data));
+
+console.log("Height of 690: " + bst.height(690));
+console.log("Depth of 690: " + bst.depth(690));
+
+console.log("\nDeleting 690...");
+bst.deleteItem(690);
+console.log("Includes 690 after delete: " + bst.includes(690));
+
+console.log("\nFinal Tree:");
 bst.prettyPrint();
